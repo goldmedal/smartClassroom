74e80b4f689833c3ac18740e726d039e1c32ee5b
goldmedal <liugs963@gmail.com>
2014-12-23 17:26:32 +0800

    manager.php update

--- manager.php
+++ manager.php
@@ -1,137 +1,143 @@
 <html>
 <head>
 	<link href="manager.css" rel="stylesheet" />
-
-	<script src="http://code.jquery.com/jquery-2.1.0.min.js"></script>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<script src="http://code.jquery.com/jquery-migrate-1.2.1.js"></script>
+  <link rel="stylesheet" href="jquery-ui.css">
+  <script src="jquery-ui.min.js"></script>
+  <script src="jquery-ui.js"></script>
 	<script type="text/javascript" src="jquery.ddslick.js"></script>
 	<script src="manager.js"></script>
-	<script type="text/javascript">
-	$(function(){
-		// 下拉選單的資料來源
-		var ddData = [
-			{
-				text: '冷氣',
-				value: 1,
-				description: '',
-				imageSrc: 'air.jpg'
-			},
-			{
-				text: '插頭',
-				value: 2,
-				description: '',
-				imageSrc: 'ele.jpg'
-			},
-			{
-				text: '開關',
-				value: 3,
-				description: '',
-				imageSrc: 'light.jpg'
-			},
-		];
- 
-		// 把 #myDropdown 轉換成 ddslick 下拉選單
-		$('.myDropdown').ddslick({
-			data: ddData,
-			width: 100,
-2
-			selectText: '請選擇'
-		});
-	});
-</script>
-	<script>
-		$(document).ready(function () {
-			$("#mask").click(hide_panel);
-			$('#back').click(function() {
-				location.href = "index.php";
-			});			
-		});
-
-	</script>
+  <script src="manager_new_device.js"></script>
 </head>
 <body>
 
 <header>
-	<div id="back">Back To List</div>	
+  <div id="back">Back To List</div> 
 	<h1>Manager UI</h1>
 </header>
 
+<!--  new Device Dialog  -->
+
+<div id="dialog-form" title="Create new equipment">
+	<p class="validateTips">All form fields are required.</p>
+	<form name="table">
+		<fieldset>
+			<label for="equip_name">Equip Name</label>
+			<input type="text" name="equip_name" id="equip_name" value="light" class="text ui-widget-content ui-corner-all">
+			<label for="equip_type">Equip Type</label>
+			<input type="text" name="equip_type" id="equip_type" value="1" class="text ui-widget-content ui-corner-all">
+			<label for="bluetooth_num">Bluetooth Number</label>
+			<input type="text" name="bluetooth_num" id="bluetooth_num" value="1" class="text ui-widget-content ui-corner-all">
+      <label for="socket_num">Socket Number</label>
+      <input type="text" name="socket_num" id="socket_num" value="1" class="text ui-widget-content ui-corner-all">
+      <label for="show">show</label>
+      yes<input type="radio" name="show" id="yes" value="Yes" class="kk" >
+      no<input type="radio" name="show" id="no"  value="No" class="kk" >
+
+			<!-- Allow form submission with keyboard without duplicating the dialog button -->
+			<input type="submit" tabindex="-1" style="position:absolute; top:-1000px" >
+		</fieldset>
+	</form>
+</div>
+
+
+
 <div id="equipTable">
-	<div class="item">
-		<span class="btNum  tb_cell">Bluetooth Group</span>
-		<span class="sockNum  tb_cell">Socket Number</span>
-		<span class="name  tb_cell">Equip Name</span>
-		<span class="type  tb_cell">Equip Type</span>
-		<span class="open  tb_cell">Open</span>
-		<span class="  tb_cell"></span>
-	</div>
-
+  <div id="users-contain" class="ui-widget">
+    <h2>Existing Devices:</h2>
+    <table id="users" class="ui-widget ui-widget-content">
+      <thead>
+        <tr class="ui-widget-header ">
+          <th>Equip Name</th>
+          <th>Equip Type</th>
+          <th>Bluetooth Number</th>
+          <th>Socket Number</th>
+          <th>Open</th>
+          <th><input type="button" id="dele" name="delete" value="delete" /></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr></tr>
 <?
-	require_once("connect.php");
+  require_once("connect.php");
 
-	$equipSQL = "SELECT * FROM `CIAlabEquip`";
-	$equipQuery = mysql_query($equipSQL) or die(mysql_error());
-	while( ($equipResult = mysql_fetch_assoc($equipQuery)) != FALSE ) {
+  $equipSQL = "SELECT * FROM `CIAlabEquip`";
+  $equipQuery = mysql_query($equipSQL) or die(mysql_error());
+  while( ($equipResult = mysql_fetch_assoc($equipQuery)) != FALSE ) {
 
-		$equipId = $equipResult['id'];
-		$group = $equipResult['group'];
-		$gNum = $equipResult['g_num'];
-		$name = $equipResult['name'];
-		$open = $equipResult['open'];
+    $equipId = $equipResult['id'];
+    $group = $equipResult['group'];
+    $gNum = $equipResult['g_num'];
+    $name = $equipResult['name'];
+    $open = $equipResult['open'];
+    $type = $equipResult['type'];
 
-?>	
-
-
-	<div class="item">
-		<span class="btNum tb_cell"><?echo $group;?></span>
-		<span class="sockNum tb_cell"><?echo $gNum;?></span>
-		<span class="name tb_cell"><? echo $name;?></span>
-		<span class="type  tb_cell"><div class="myDropdown" id="num1"></div></span>
-		<span class="open tb_cell">
-		<? 
-			if ($open == 0) echo "Close";
-			else echo "Open";
-		?>
-		</span>
-		<span class="edit tb_cell">Edit</span>
-	</div>		
+?>          
+        <tr class='row' id='row'>
+          <td><?echo $name; ?></td>
+          <td>
 <?
-	}
+            switch ($type) {
+              case 1:
+                echo "插座";
+                break;
+
+              case 2:
+                echo "電器";
+                break;
+
+              case 3:
+                echo "紅外線";
+                break;
+
+              default:
+                # code...
+                break;
+            }
 ?>
+          </td>
+          <td><? echo $group; ?></td>
+          <td><? echo $gNum; ?></td>
+          <td><? echo $open; ?></td>
+          <td> <input type='checkbox' name='del' /></td>
+        </tr>  
+<?
+   }
+?>              
+      </tbody>
+    </table>
+  </div>
+  <button id="create-user">Create new equipment</button>
+
 </div>
 
-<div id="btTable">
-	<div class="item">
-		<span class="btNum  tb_cell">Bluetooth Number</span>
-		<span class="macAddress  tb_cell">Mac Address</span>
-	</div>
 
+ <!-- Bluetooth Device Table    -->
+
+
+
+
+<div id="bluetooth_table">
+  <b>Bluetooth Number:</b><br>
+  <input type="text" id="bluetext" value="input bluetooth number">
+  <button id="add_blue" >add</button>
+  <button id="dele_blue" >delete selected bluetooth</button>
+  <br><br>
+  <ol>
 <?
 
-	$equipSQL = "SELECT * FROM `btDevice`";
-	$equipQuery = mysql_query($equipSQL) or die(mysql_error());
-	while( ($equipResult = mysql_fetch_assoc($equipQuery)) != FALSE ) {
+  $equipSQL = "SELECT * FROM `btDevice`";
+  $equipQuery = mysql_query($equipSQL) or die(mysql_error());
+  while( ($equipResult = mysql_fetch_assoc($equipQuery)) != FALSE ) {
 
-		$btId = $equipResult['id'];
-		$MacAddress = $equipResult['MacAddress'];
+    $btId = $equipResult['id'];
+    $MacAddress = $equipResult['MacAddress'];
+    echo "<li>".$MacAddress."<input type='checkbox' name='del' /></li>";
 
-?>	
-
-
-	<div class="item">
-		<span class="btNum tb_cell"><?echo $btId;?></span>
-		<span class="macAddress tb_cell"><?echo $MacAddress;?></span>
-	</div>		
-<?
-	}
-?>
-</div>
-
-
-<div id="mask">	
-</div>
-
-<div id="editPanel">
+  }
+?>    
+  </ol>
 </div>
 
 
