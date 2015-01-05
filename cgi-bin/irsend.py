ca8774e88cc87d84ab54b32fd9bcddf1ecb41966
goldmedal <liugs963@gmail.com>
2015-01-05 17:17:06 +0800

    ir arduino and ir python cgi

--- cgi-bin/irsend.py
+++ cgi-bin/irsend.py
@@ -5,13 +5,27 @@ print
 
 import bluetooth
 import cgi , cgitb
+import MySQLdb
 
+form = cgi.FieldStorage()
+sign = str(form.getvalue('sign'))
+deviceID = str(form.getvalue('id'))
+
+db = MySQLdb.connect(host="localhost", user="root", passwd="1234", db="smart_classroom")
+cursor = db.cursor()
+
+sql = "SELECT * FROM `" + classID + "Equip` WHERE `id` = '" + deviceID + "'"
+cursor.execute(sql)
+
+result = cursor.fetchall()
+
+for record in result :
+	bd_addr = record[4]
 
 port = 1
-bd_addr = "98:D3:31:B4:09:A9"
 
 sock = bluetooth.BluetoothSocket ( bluetooth.RFCOMM )
 sock.connect ((bd_addr, port))
-sock.send('111')
-sock.close
+sock.send(sign)
 
+sock.close()
